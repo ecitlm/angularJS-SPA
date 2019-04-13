728c09cd87385bbc0c28c01d259b63f515146b0a
ecitlm <ecitlm@163.com>
2019-04-13 19:55:43 +0800

    feat:add storage.js

--- views/mine/mine.js
+++ views/mine/mine.js
@@ -4,8 +4,13 @@ define(['app', 'filters'], function (app, filters) {
     $scope.title = 'this is the first mine page'
     console.log('parent', $scope.$parent.list)
     $scope.$parent.list.active = '3'
+    // ng-init 初始化
     $scope.test = function () {
       console.log('ng init')
+    }
+    // 获取ng-click 事件DOM
+    $scope.btnEvent = function ($event) {
+      console.log($event.target)
     }
   })
 })
