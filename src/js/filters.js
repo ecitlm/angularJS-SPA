7ea415a8c26c730e83ecc524ef39f509675ad84e
ecitlm <ecitlm@163.com>
2019-04-16 19:56:23 +0800

    docs:介绍完善

--- src/js/filters.js
+++ src/js/filters.js
@@ -2,7 +2,7 @@
 define(['app'], function (app) {
   app.filter('test', function () {
     return function (params) {
-      return params + '== filter is work'
+      return params + 'filter运行成功 '
     }
   })
 })
--- views/index/index.js
+++ views/index/index.js
@@ -1,7 +1,7 @@
 /* global define */
 define(['app', 'filters'], function (app, filters) {
   app.registerController('IndexController', function ($scope, $rootScope, params, $stateParams, $state) {
-    $scope.title = 'this is the first home page'
+    $scope.title = '欢迎使用angularJs'
     console.log($stateParams, $state)
     console.log($state)
     console.log($stateParams, $scope, $scope.$parent.list, params)
